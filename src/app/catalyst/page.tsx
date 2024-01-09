5966dbe35969d78c2150753c16d869401bf60517
jamesdunseith <jdunseith@gimbalabs.io>
2024-01-09 09:15:36 -0500

    Add just enough responsive design to / and /catalyst

--- src/app/catalyst/page.tsx
+++ src/app/catalyst/page.tsx
@@ -1,9 +1,9 @@
 export default function CatalystPage() {
   return (
-    <div className="bg-primary text-primary-content p-5 w-2/3 mx-auto my-5 shadow-lg">
+    <div className="bg-primary text-primary-content p-5 w-full md:w-2/3 mx-auto my-5 shadow-lg">
       <h1 className="text-4xl py-5">Catalyst Fund 11 Proposals</h1>
       <p>The Andamio team has two proposals in Catalyst Fund 11:</p>
-      <div className="grid grid-cols-2 gap-5 pt-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pt-5">
         <div className="bg-secondary text-secondary-content p-3 rounded-lg border border-primary flex flex-col">
           <h2 className="text-xl pb-3 font-mono">Andamio (Core Team): Learning & Contribution Platform</h2>
           <p className="py-2 tx-sm border-t border-primary mt-2">PROBLEM STATEMENT:</p>
--- src/components/ui/GlobalNav.tsx
+++ src/components/ui/GlobalNav.tsx
@@ -6,38 +6,39 @@ import Link from "next/link";
 import { andamioConfig } from "../../andamio/config";
 
 const GlobalNav = () => (
-  <div className="card bg-primary text-primary-content font-mono shadow-xl mx-5 mt-5 z-10">
+  <div className="card bg-primary text-primary-content font-mono shadow-xl md:mx-5 md:mt-5 z-10">
     <div className="navbar">
-      <div className="flex-1">
+      <div className="flex justify-between">
         <Link className="btn btn-ghost normal-case text-lg" href="/">
           <Image src="/andamio.png" height={40} width={40} alt="andamio" />
-          {andamioConfig.title}
+          <p className="hidden md:block">{andamioConfig.title}</p>
         </Link>
       </div>
       <ul className="menu menu-horizontal px-1">
         <li className="text-info font-bold">
           <Link href="/catalyst">Catalyst Fund 11</Link>
         </li>
-        <li>
+        <li className="hidden md:block">
           <Link href={"/course"}>{`Start ${andamioConfig.title} Course`}</Link>
         </li>
-        <li>
+        {/* <li>
           <Link href={"/course-management/roles/learner/dashboard"}>Learner Dashboard</Link>
-        </li>
+        </li> */}
         {process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes("cm") && (
-        <li>
-          <Link href={"/course-management"}>Course Management</Link>
-        </li>
+          <li className="hidden md:block">
+            <Link href={"/course-management"}>Course Management</Link>
+          </li>
         )}
         {process.env.NEXT_PUBLIC_EXPERIMENTAL_FEATURES?.split(",").includes("pm") && (
-        <li>
-          <Link href={"/contributor-platform"}>Contributor Platform</Link>
-        </li>
+          <li className="hidden md:block">
+            <Link href={"/contributor-platform"}>Contributor Platform</Link>
+          </li>
         )}
       </ul>
       <div className="ml-5">
-      <CardanoWallet />
-
+        <div className="hidden md:block">
+          <CardanoWallet />
+        </div>
       </div>
     </div>
   </div>
